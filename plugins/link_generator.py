4776e35a17df26b63d1cd72d0adfee8d1d6f069f
whoami847 <mrbeastbangla069@gmail.com>
2025-05-27 22:42:05 +0600

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -571,7 +571,7 @@ async def handle_format_input(client: Client, message: Message):
             )
         
     except Exception as e:
-        logger.error(to_small_caps_with_html(f"error in handle_format_input: {e}"))        )
+        logger.error(to_small_caps_with_html(f"error in handle_format_input: {e}"))        
         await message.reply_text(
             to_small_caps_with_html(f"<b>━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>\n\n<b>❌ An error occurred while processing format.</b>\n\n<b>━━━━━━━━━━━━━━━━</b>"), 
             parse_mode=ParseMode.HTML
