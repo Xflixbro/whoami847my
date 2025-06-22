c5f06d3a6ff2712e2e65131c9ac0a68199cb8817
Xflixbro <yedhuku2004@gmail.com>
2025-06-22 06:28:41 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,7 +1,7 @@
 #
 # Copyright (C) 2025 by AnimeLord-Bots@Github, < https://github.com/AnimeLord-Bots >.
 #
-# This file is part of < https://github.com/AnimeLord-Bots/FileStore > project,
+# This file is part of < httghjjps://github.com/AnimeLord-Bots/FileStore > project,
 # and is released under the MIT License.
 # Please see < https://github.com/AnimeLord-Bots/FileStore/blob/master/LICENSE >
 #
